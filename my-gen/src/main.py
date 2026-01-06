fcb2858111bcb92320ec45800cfa11ef13221e67
endermend <endermend05@gmail.com>
2026-01-07 01:34:44 +1000

    Nailed it

--- my-gen/src/main.py
+++ my-gen/src/main.py
@@ -8,6 +8,19 @@ from sqlalchemy.exc import OperationalError
 from . import generate_nick, generate_event
 from . import *
 
+import logging
+import sys
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger(__name__)
+
 events_cnt = 0
 players_cnt = 0
 
@@ -47,21 +60,26 @@ def update_player_event(session, player_id, event_id):
     else:
         player_event.repeats += 1
 
+def get_random_event():
+    global events_cnt
+    for i in range(events_cnt):  
+        event_id = randint(1, events_cnt)
+        event = session.query(Events).filter_by(id = event_id).first()
+        if event is not None and not event.special:
+            return event
 
 def add_player_event(session):
     global events_cnt
     global players_cnt
-
     if events_cnt == 0 or players_cnt == 0:
         return
     player_id = randint(1, players_cnt)
-    event_id = randint(1, events_cnt)
     player = session.query(Players).filter_by(id = player_id).first()
-    event = session.query(Events).filter_by(id = event_id).first()
-    if player is None or event is None:
+    event = get_random_event()
+    if event is None:
         return
-    update_player_event(session, player_id, event_id)
-    exp_gained = event.exp_gained.copy() + player.exp
+    update_player_event(session, player_id, event.id)
+    exp_gained = event.exp_gained + player.exp
     player.exp = 0
     while exp_gained > 0:
         level = session.query(Levels).filter_by(level = player.level).first()
@@ -71,25 +89,31 @@ def add_player_event(session):
             break
         exp_gained -= level.lvlup_exp
         player.level += 1
-        if next_level.assigned_event is None:
+        if event_id := next_level.assigned_event is None:
             continue
-        event = session.query(Events).filter_by(id = next_level.assigned_event).first()
-        update_player_event(session, player_id, event.id)
-        exp_gained += event.exp_gained
+        event = session.query(Events.exp_gained).filter_by(id = event_id).first()
+        update_player_event(session, player_id, event_id)
+        exp_gained += event
 
 def add_levels(session):
     if session.query(func.count(Levels.level)).scalar() > 0:
         return
-    event = Events(id=10000000, name='Its yours anniversary!', exp_gained=1000)
+    event = Events(name='Its yours anniversary!', exp_gained=1000, special=True)
     session.add(event)
+    session.commit()
+    exp_level = lambda i: int((100+383*(i-1))*(2**((i-1)/3)))
+    logger.info(f'special_event: {event.id}')
     for i in range(1, 21):
-        level = Levels(level=i, lvlup_exp=100 + 12 * (i - 1), assigned_event=10000000 if i % 10 == 0 else None)
+        assigned_event = None if i % 10 != 0 else event.id
+        level = Levels(level=i, lvlup_exp=exp_level(i), assigned_event=assigned_event)
         session.add(level)
 
 def loop(session):
     global events_cnt
     global players_cnt
     add_levels(session)
+    players_cnt = session.query(func.count(Players.id)).scalar()
+    events_cnt = session.query(func.count(Events.id)).scalar()
     events = [add_event, add_player, add_player_event]
     while True:
         choices(events, [0.2, 0.1, 0.7])[0](session)
--- my-gen/src/model.py
+++ my-gen/src/model.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Integer, String, DateTime, SmallInteger, ForeignKey, func
+from sqlalchemy import Integer, String, DateTime, SmallInteger, ForeignKey, func, Boolean
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, mapped_column
@@ -14,7 +14,8 @@ class Events(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(255), nullable=False)
-    exp_gained: Mapped[int]  = mapped_column(Integer, nullable=False, default=0)
+    exp_gained: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    special: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
 
 
 class Levels(Base):
@@ -39,5 +40,5 @@ class PlayerEvents(Base):
 
     player: Mapped[int] = mapped_column(ForeignKey(Players.id), primary_key=True)
     event: Mapped[int] = mapped_column(ForeignKey(Events.id), primary_key=True)
-    updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, server_default=func.now(), onupdate=func.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=func.now(), server_default=func.now(), onupdate=func.now())
     repeats: Mapped[int] = mapped_column(Integer, nullable=False, default=1)
